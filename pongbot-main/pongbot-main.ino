fe57e6bbc5f00ad509e70dbef609bf693a29e7f3
Arkady Arkhangorodsky <aarkhangorodsky@gmail.com>
2016-01-18 20:16:58 -0500

    Implemented lidar reading and aim

--- pongbot-main/pongbot-main.ino
+++ pongbot-main/pongbot-main.ino
@@ -17,6 +17,11 @@ NICE TO HAVE:
 */
 
 #include <Servo.h>
+#include <Wire.h>
+#define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
+#define    RegisterMeasure     0x00          // Register to write to initiate ranging.
+#define    MeasureValue        0x04          // Value to initiate ranging.
+#define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 
 // Angle between leftmost edge and rightmost edge
 #define  SCAN_ANGLE   100
@@ -46,22 +51,55 @@ void initialize_sweep() {
 // Function to sweep from left to right. Returns [distance, angle] of closest cup
 float *perform_sweep() {
   // initialize variables to keep track of
-  float *closest_cup; // [distance, angle]
-
+  float *closest_cup = (float *) malloc(sizeof(float) * 2);
+  closest_cup[0] = 100000;
+  
   //initialize_sweep(); // Set current angle to be left_edge
   // for loop to go from left edge to right edge, reading from sensor every time
-    for(servoPos = left_edge; servoPos <= right_edge; servoPos += 1) // goes from 0 degrees to 180 degrees 
-    {                                  // in steps of 1 degree 
-      servo.writeMicroseconds(servoPos);              // tell servo to go to position in variable 'pos' 
-      // Read from lidar
-      // Check if reading is closer than what we've seen
-      delay(10);                       // waits 10ms for the servo to reach the position 
-     }
+  for(servoPos = left_edge; servoPos <= right_edge; servoPos += 1) // goes from 0 degrees to 180 degrees 
+  {                                                                // in steps of 1 degree 
+    servo.writeMicroseconds(servoPos);              // tell servo to go to position in variable 'pos' 
+    delay(10);                       // waits 10ms for the servo to reach the position 
+    
+    // Read from lidar
+    int reading = read_lidar();
 
-  
-  // closest_cup[0] = closest_distance;
-  // closest_cup[1] = best_angle;
+      // Check if reading is closer than what we've seen  
+      if (reading < closest_cup[0]) {
+        closest_cup[0] = reading;
+        closest_cup[1] = servoPos;
+      }
+    
+   }
+
   return closest_cup;
+}
+
+int read_lidar() {
+      int reading = 0;
+    
+      Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+      Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
+      Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
+      Wire.endTransmission(); // stop transmitting
+    
+      delay(20); // Wait 20ms for transmit
+    
+      Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+      Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
+      Wire.endTransmission(); // stop transmitting
+    
+      delay(20); // Wait 20ms for transmit
+    
+      Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
+    
+      if(2 <= Wire.available()) // if two bytes were received
+      {
+        reading = Wire.read(); // receive high byte (overwrites previous reading)
+        reading = reading << 8; // shift high byte to be high 8 bits
+        reading |= Wire.read(); // receive low byte as lower 8 bits
+        return reading;
+      }
 }
 
 // Function to shoot at a given [distance, angle] target
@@ -69,9 +107,14 @@ void shoot(float *closest_cup) {
   float distance = closest_cup[0];
   float angle = closest_cup[1];
     
-  // aim(angle) // rotate to point in the right direction
+  aim(angle); // rotate to point in the right direction
   // spin_shooting_motors(distance) // calculate required speed of shooting motors
   // push_ball() // talk to solenoid, trigger pushing the ball
+}
+
+void aim (float angle){
+    servo.writeMicroseconds(angle);
+    delay(100);                       // waits 10ms for the servo to reach the position 
 }
 
 
